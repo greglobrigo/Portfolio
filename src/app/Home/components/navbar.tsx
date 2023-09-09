655190a4fc9453dfdf2dbb90d95809f0172cc8d9
Greg <golivefun@gmail.com>
2023-09-09 23:13:55 +0800

    Added responsive navbar.

--- src/app/Home/components/navbar.tsx
+++ src/app/Home/components/navbar.tsx
@@ -15,22 +15,37 @@ export function Navbar() {
     ]
     return (
         <>
-            <div className="flex">
+            <nav className="flex bg-gray-700">
                 <div className="flex w-full pl-16">
                     <h1>Logo</h1>
                 </div>
-                <div className="flex w-full justify-around">
-                    {navItems.map((item, index) => {
-                        return (
-                            <div key={index}>
-                                <a href={item.link}>
-                                    {item.name}
-                                </a>
-                            </div>
-                        )
-                    })}
+
+                <div className="sm:block lg:hidden px-2 hover:cursor-pointer border rounded-md">
+                    <div className="w-10 h-1 bg-black my-2"
+                    ></div>
+                    <div className="w-10 h-1 bg-black my-2"
+                    ></div>
+                    <div className="w-10 h-1 bg-black my-2"
+                    ></div>
                 </div>
-            </div>
+
+                <div className="sm:hidden lg:block w-full">
+                    <ul className="flex justify-around">
+                        {navItems.map((item, index) => {
+                            return (
+                                <li key={index}>
+                                    <a
+                                    // href={item.link}
+                                    >
+                                        {item.name}
+                                    </a>
+                                </li>
+                            )
+                        })}
+                    </ul>
+                </div>
+
+            </nav>
         </>
     )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,11 @@ const config: Config = {
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
+    screens: {
+      'sm': '360px',
+      'md': '768px',
+      'lg': '1024px',
+    },
   },
   plugins: [],
 }
