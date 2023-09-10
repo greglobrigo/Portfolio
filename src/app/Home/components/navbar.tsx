f9ce102462c9d52d101a79760d37e4790c4fd550
greglobrigo <golivefun@gmail.com>
2023-09-10 20:20:41 +0800

    Added pictures and support for dark mode.

--- src/app/Home/components/navbar.tsx
+++ src/app/Home/components/navbar.tsx
@@ -22,6 +22,21 @@ export function Navbar() {
         }
     ]
     const [navOpen, setNavOpen] = useState<boolean>(false)
+    const [darkMode, setDarkMode] = useState<boolean>(false)
+
+    const toggleDarkMode = () => {
+        setDarkMode(!darkMode)
+        if (!darkMode) {
+            document.documentElement.style.setProperty('--foreground-rgb', '255, 255, 255')
+            document.documentElement.style.setProperty('--background-start-rgb', '0, 0, 0')
+            document.documentElement.style.setProperty('--background-end-rgb', '0, 0, 0')
+        } else {
+            document.documentElement.style.setProperty('--foreground-rgb', '0, 0, 0')
+            document.documentElement.style.setProperty('--background-start-rgb', '214, 219, 220')
+            document.documentElement.style.setProperty('--background-end-rgb', '255, 255, 255')
+        }
+    }
+
     useEffect(() => {
         window.addEventListener('resize', () => {
             setNavOpen(false)
@@ -51,7 +66,8 @@ export function Navbar() {
                             <path strokeLinecap="round" strokeLinejoin="round" d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z" />
                         </svg>
                         <label className="relative inline-flex items-center cursor-pointer">
-                            <input type="checkbox" value="" className="sr-only peer" />
+                            <input onClick={toggleDarkMode}
+                            type="checkbox" value={`darkMode`} className="sr-only peer" />
                             <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
                         </label>
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -1,18 +1,41 @@
 import { Navbar } from "./components/navbar";
+import { useState, useRef, useEffect } from 'react'
+import autoAnimate from '@formkit/auto-animate'
 
 export function Home() {
+    // const [show, setShow] = useState(false)
+    // const parent = useRef(null)
+
+    // useEffect(() => {
+    //     parent.current && autoAnimate(parent.current)
+    // }, [parent])
+
+    // const reveal = () => setShow(!show)
+
     return (
         <>
-            <Navbar/>
+            <Navbar />
             <main>
-                <section className="flex flex-row items-center justify-around h-[90vh]">
-                    <div className="flex flex-col items-center justify-center h-full">
-                        <h1 className="text-4xl font-bold">Hello, I'm Greg.</h1>
-                        <h2 className="text-2xl font-semibold">I'm a full-stack developer.</h2>
+                <section className="flex flex-col h-[90vh]">
+                    <div className="nn:flex-col-reverse sm:flex-row nn:justify-center sm:justify-around flex items-center h-full w-full ">
+                        <div className="flex-row nn:pt-8 sm:pt-0">
+                            <h1 className="text-6xl font-bold">Hello, I'm Greg.</h1>
+                            <h2 className="text-4xl font-semibold">I'm a full-stack developer.</h2>
+                        </div>
+                        <div className="flex nn:pb-8 sm:pt-0">
+                            <img src='/selfie.jpg' alt="aws" className="w-[240px] h-[320px] rounded-[50%] object-fill shadow-2xl" />
+                        </div>
                     </div>
-                    <div className="flex flex-col items-center justify-center h-full">
-                        <h1 className="text-4xl font-bold">Hello, I'm Greg.</h1>
-                        <h2 className="text-2xl font-semibold">I'm a full-stack developer.</h2>
+                </section>
+                <section className="flex flex-col h-[90vh]">
+                    <div className="nn:flex-col-reverse sm:flex-row nn:justify-center sm:justify-around flex items-center h-full w-full ">
+                        <div className="flex-row nn:pt-8 sm:pt-0">
+                            <h1 className="text-4xl font-bold">Hello, I'm Greg.</h1>
+                            <h2 className="text-2xl font-semibold">I'm a full-stack developer.</h2>
+                        </div>
+                        <div className="flex nn:pb-8 sm:pt-0">
+                            <img src='/selfie.jpg' alt="aws" className="w-[240px] h-[320px] rounded-[50%] object-fill shadow-2xl" />
+                        </div>
                     </div>
                 </section>
             </main>
