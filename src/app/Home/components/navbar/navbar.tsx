f12f9bb072fa4ff351fca5a1978fe65cda403ec5
greglobrigo <golivefun@gmail.com>
2023-09-17 23:21:37 +0800

    Altered nav items description.

--- src/app/Home/components/navbar/navbar.tsx
+++ src/app/Home/components/navbar/navbar.tsx
@@ -14,10 +14,10 @@ export default function Navbar () {
         </div>
         <div className="gpt3__navbar-links_container">
           <p><a href="#home">Home</a></p>
-          <p><a href="#wgpt3">What is GPT3?</a></p>
-          <p><a href="#possibility">Open AI</a></p>
-          <p><a href="#features">Case Studies</a></p>
-          <p><a href="#blog">Library</a></p>
+          <p><a href="#wgpt3">About</a></p>
+          <p><a href="#possibility">Projects</a></p>
+          <p><a href="#features">Contact</a></p>
+          <p><a href="#blog">Resume</a></p>
         </div>
       </div>
       <div className="gpt3__navbar-menu">
@@ -28,10 +28,10 @@ export default function Navbar () {
         <div className="gpt3__navbar-menu_container scale-up-center">
           <div className="gpt3__navbar-menu_container-links">
             <p><a href="#home">Home</a></p>
-            <p><a href="#wgpt3">What is GPT3?</a></p>
-            <p><a href="#possibility">Open AI</a></p>
-            <p><a href="#features">Case Studies</a></p>
-            <p><a href="#blog">Library</a></p>
+            <p><a href="#wgpt3">About</a></p>
+            <p><a href="#possibility">Projects</a></p>
+            <p><a href="#features">Contact</a></p>
+            <p><a href="#blog">Resume</a></p>
           </div>
         </div>
         )}
