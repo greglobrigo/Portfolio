9591a4e80d754a47313011423bd48fbb9ed01950
greglobrigo <golivefun@gmail.com>
2023-10-11 14:06:12 +0800

    Added animations to about me section.

--- src/app/About/components/picture.tsx
+++ src/app/About/components/picture.tsx
@@ -1,5 +1,6 @@
 'use client';
 import '../About.css'
+import { Fade } from 'react-awesome-reveal'
 import { useState } from 'react'
 import Image from 'next/image'
 
@@ -8,29 +9,34 @@ export default function AboutPic() {
     const [show, setShow] = useState(false)
     return (
         <>
-            <div onMouseEnter={() => setShow(true)} onMouseLeave={() => setShow(false) } onClick={()=>setShow(!show)} className='cursor-pointer'>
-                    {
-                        show ?
+            <div onMouseEnter={() => setShow(true)} onMouseLeave={() => setShow(false)} onClick={() => setShow(!show)} className='cursor-pointer'>
+                {
+                    show ?
+                        <Fade direction="up" >
                             <div>
                                 <div className="bg-[#3d71df] rounded-lg shadow-lg aws">
                                     <Image width={275} height={600}
-                                    className="w-auto h-auto opacity-80" src="/gym.jpg" alt="gym" />
+                                        className="w-auto h-auto opacity-80" src="/gym.jpg" alt="gym" />
                                 </div>
                                 <div className="flex justify-center items-center pt-8">
                                     <p className="text-xl font-semibold gradient__text">Gym</p>
                                 </div>
                             </div>
-                            :
+                        </Fade>
+                        :
+                        <Fade direction="up" >
                             <div>
                                 <div className="bg-[#3d71df] rounded-lg shadow-lg aws">
                                     <Image width={275} height={600}
-                                    className="w-auto h-auto opacity-80" src="/aws.jpg" alt="aws training" />
+                                        className="w-auto h-auto opacity-80" src="/aws.jpg" alt="aws training" />
                                 </div>
                                 <div className="flex justify-center items-center pt-8">
                                     <p className="text-xl font-semibold gradient__text">AWS Training</p>
                                 </div>
                             </div>
-                    }
+                        </Fade>
+
+                }
             </div >
         </>
     )
--- src/app/About/index.tsx
+++ src/app/About/index.tsx
@@ -1,34 +1,50 @@
+'use client'
 import './About.css'
 import AboutPic from './components/picture'
+import { Fade } from 'react-awesome-reveal'
 
 export default function About() {
     return (
         <>
             <div className="about__header section__padding gradient__bg__left" id="about">
                 <div className="about__header-content">
-                    <h1 className="gradient__text">About me</h1>
-                    <p className="text-2xl font-semibold">
-                        I graduated from College in Divine Word College of Legazpi. I have a bachelor&apos;s
-                        degree in <span className="gradient__text2">Business and Finance</span>
-                    </p>
-                    <p className="text-2xl font-semibold">
-                        I took a coding bootcamp focusing on React and Ruby on Rails in <a href='https://www.avionschool.com/' target="_blank" rel="noreferrer" className="gradient__text2"><span className="gradient__text2">Avion School</span></a>
-                    </p>
-                    <p className="text-2xl font-semibold">
-                        Before getting into a developer role, I worked as a <span className="gradient__text2">Technical Support Representative</span> for <span className="gradient__text2">3 years,</span>
-                        for companies like <span className="gradient__text2">Lyft</span> and <span className="gradient__text2">Walmart</span>
-                    </p>
-                    <p className="text-2xl font-semibold">
-                        I am currently working as a <span className="gradient__text2">Software Engineer</span> at <span className="gradient__text2">Mosaic Solutions</span> for almost 2 years.
-                    </p>
-                    <p className="text-2xl font-semibold">
-                        I&apos;ve been progamming for 3 years, and have built and led projects
-                        for companies such as <span className="gradient__text2">SM Supermalls</span> and <span className="gradient__text2">TMG (The Moment Group of Companies)</span>
-                    </p>
-                    <p className="text-2xl font-semibold">
-                        I try my best to stay fit by going to the
-                        <span className="gradient__text2"> gym </span> whenever I can.
-                    </p>
+                    <Fade direction="up" >
+                        <h1 className="gradient__text">About me</h1>
+                    </Fade>
+                    <Fade direction="up" delay={200}>
+                        <p className="text-2xl font-semibold">
+                            I graduated from College in Divine Word College of Legazpi. I have a bachelor&apos;s
+                            degree in <span className="gradient__text2">Business and Finance</span>
+                        </p>
+                    </Fade>
+                    <Fade direction="up" delay={400}>
+                        <p className="text-2xl font-semibold">
+                            I took a coding bootcamp focusing on React and Ruby on Rails in <a href='https://www.avionschool.com/' target="_blank" rel="noreferrer" className="gradient__text2"><span className="gradient__text2">Avion School</span></a>
+                        </p>
+                    </Fade>
+                    <Fade direction="up" delay={600}>
+                        <p className="text-2xl font-semibold">
+                            Before getting into a developer role, I worked as a <span className="gradient__text2">Technical Support Representative</span> for <span className="gradient__text2">3 years,</span>
+                            for companies like <span className="gradient__text2">Lyft</span> and <span className="gradient__text2">Walmart</span>
+                        </p>
+                    </Fade>
+                    <Fade direction="up" delay={800}>
+                        <p className="text-2xl font-semibold">
+                            I am currently working as a <span className="gradient__text2">Software Engineer</span> at <span className="gradient__text2">Mosaic Solutions</span> for almost 2 years.
+                        </p>
+                    </Fade>
+                    <Fade direction="up" delay={1000}>
+                        <p className="text-2xl font-semibold">
+                            I&apos;ve been progamming for 3 years, and have built and led projects
+                            for companies such as <span className="gradient__text2">SM Supermalls</span> and <span className="gradient__text2">TMG (The Moment Group of Companies)</span>
+                        </p>
+                    </Fade>
+                    <Fade direction="up" delay={1200}>
+                        <p className="text-2xl font-semibold">
+                            I try my best to stay fit by going to the
+                            <span className="gradient__text2"> gym </span> whenever I can.
+                        </p>
+                    </Fade>
                 </div>
                 <div className="about__header-image gradient__bg__center">
                     <AboutPic />
