8e2e033daa5917e2803b7cd8d5e85e4b3de3f05d
greglobrigo <golivefun@gmail.com>
2023-11-12 15:01:11 +0800

    Added Contacts to portfolio.

--- src/app/Contact/index.tsx
+++ src/app/Contact/index.tsx
@@ -7,11 +7,76 @@ import Image from 'next/image'
 export default function Contact() {
     return (
         <>
-            <div className="personal__header section__padding gradient__bg__left" id="personal">
-                <div className="personal__header-content">
+            <div className="contact__header section__padding gradient__bg__left" id="contact">
+                <div className="contact__header-content">
                     <Fade triggerOnce direction='up' delay={200}>
-                        <h1 className="gradient__text pb-2" id="projects">Contact</h1>
+                        <h1 className="gradient__text" id="projects">Contact</h1>
                     </Fade>
+                    <Fade triggerOnce direction='up' delay={200}>
+                        <p className='pb-4'>
+                            You can contact me through the following:
+                        </p>
+                    </Fade>
+                    <ul className="skills__header-content__list">
+                        <Fade triggerOnce direction="up" delay={200}>
+                            <li>
+                                <div className='flex items-center'>
+                                    <h2 className="gradient__text2">
+                                        Viber
+                                    </h2>
+                                    <Image width={75} height={50} className="w-[75px] h-[50px]"
+                                        src="/viber-logo.png" alt="viber" />
+                                </div>
+                                <p className='pb-4'>
+                                    +(63) 945-321-7121 (Greg)
+                                </p>
+                            </li>
+                        </Fade>
+                        <Fade triggerOnce direction="up" delay={200}>
+                            <li>
+                                <div className='flex items-center gap-2'>
+                                    <h2 className="gradient__text2">
+                                        Whatsapp
+                                    </h2>
+                                    <Image width={50} height={50} className="w-[50px] h-[50px] mt-1"
+                                        src="/whatsapp.svg.webp" alt="whatsapp" />
+                                </div>
+                                <p className='pb-4'>
+                                    +(63) 945-321-7121 (Greg)
+                                </p>
+                            </li>
+                        </Fade>
+                        <Fade triggerOnce direction="up" delay={200}>
+                            <li>
+                                <div className='flex items-center gap-2'>
+                                    <h2 className="gradient__text2">
+                                        Linkedin
+                                    </h2>
+                                    <Image width={45} height={45} className="w-[45px] h-[45px] mt-1 ml-1"
+                                        src="/linkedin-logo.png" alt="linkedin" />
+                                </div>
+                                <a href="https://www.linkedin.com/in/greglobrigo/" target="_blank" rel="noreferrer">
+                                    <p className='pb-4 hover:underline'>
+                                        https://www.linkedin.com/in/greglobrigo/
+                                    </p>
+                                </a>
+                            </li>
+                        </Fade>
+                        <Fade triggerOnce direction="up" delay={200}>
+                            <li>
+                                <div className='flex items-center gap-2'>
+                                    <h2 className="gradient__text2">
+                                        Gmail
+                                    </h2>
+                                    <Image width={50} height={40} className="w-[50px] h-[40px] ml-1"
+                                        src="/gmail.svg.png" alt="gmail" />
+                                </div>
+                                <p className='pb-4 hover:underline'>
+                                    greglobrigojr@gmail.com
+                                </p>
+                            </li>
+                        </Fade>
+                    </ul>
                 </div>
             </div>
         </>
