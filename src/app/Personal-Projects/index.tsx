c3530d66e39a5f265eaabb9c0afc94037c0a774b
greglobrigo <golivefun@gmail.com>
2023-11-12 13:14:25 +0800

    Added tictactoe to projects.

--- src/app/Personal-Projects/index.tsx
+++ src/app/Personal-Projects/index.tsx
@@ -56,7 +56,7 @@ export default function PersonalProjects() {
                         Tags: Web App, Websockets, Chat, Social Media, Real Time Messaging
                     </p>
                     <p>
-                        Technologies: ReactJS, NextJS, Websockets, Ruby on Rails, PostgreSQL, Vercel, Render
+                        Technologies: ReactJS, NextJS, Typescript, Websockets, Ruby on Rails, PostgreSQL, Vercel, Render
                     </p>
                     <p className='pb-[100px]'>
                         Description: ChatScape is a web app that I built to reinforce my knowledge on websockets and to improve my frontend skills.
@@ -108,77 +108,45 @@ export default function PersonalProjects() {
                         Description: Being fond of Asus, especially their ROG Laptop products, I decided to create a landing page for it.
                         The page was made way back in 2021.
                     </p>
-                    {/* <h2 className="gradient__text2 leading-none pb-2" id="tmg">
-                        • The Grid Food Market
-                    </h2>
-                    <div className='flex flex-col xl:flex-row items-center gap-10'>
-                        <Image alt="the-grid-login" src='/thegrid-login.png' width={500} height={500}
-                            onClick={() => window.open('/thegrid-login.png')}
-                            className='cursor-pointer hover:scale-110 transition duration-300 ease-in-out rounded-sm'
-                        />
-                        <Image alt="the-grid-ui" src='/thegrid-ui.png' width={500} height={500}
-                            className='cursor-pointer hover:scale-110 transition duration-300 ease-in-out rounded-sm border-[#e6e5eb6c]
-                            border-2'
-                            onClick={() => window.open('/thegrid-ui.png')}
-                        />
-                    </div>
-                    <p>
-                        Role: Lead Developer (Team of 2)
-                    </p>
-                    <p>
-                        Timeline: October 2023 - Present
-                    </p>
-                    <p>
-                        Project Type: Web App, ETL, Automated Reports Generation
-                    </p>
-                    <p>
-                        Tags: Fullstack Development, Web App, ETL, Automated Reports Generation, Data Pipeline, Data Aggregation, Data Monitoring, Data Analytics, Data Engineering
-                    </p>
-                    <p>
-                        Technologies: ReactJS, NextJS, ExcelJS, PostgreSQL, Node, Serverless, TypeORM, AWS Services
-                    </p>
-                    <p className='pb-[100px]'>
-                        Description: Data IQ The Grid is another project that I&apos;m currently working on.
-                        The web app provides visibility to the operations team of The Grid Food Market by providing them with real time sales status reports
-                        with regards to their tenants and other data that they need to monitor on a daily basis.
-                        We&apos;ve also created a portal for them to download sales reports on demand.
-                    </p>
                     <h2 className="gradient__text2 leading-none pb-2" id="tmg">
-                        • Generic Scheduler
+                        TicTacToe
                     </h2>
                     <div className='flex flex-col xl:flex-row items-center gap-10'>
-                        <Image alt="scheduler1" src='/scheduler1.png' width={500} height={500}
-                            onClick={() => window.open('/scheduler1.png')}
+                        <Image alt="tictactoe1" src='/tictactoe1.png' width={500} height={500}
+                            onClick={() => window.open('/tictactoe1.png')}
                             className='cursor-pointer hover:scale-110 transition duration-300 ease-in-out rounded-sm'
                         />
-                        <Image alt="scheduler2" src='/scheduler2.png' width={500} height={500}
-                            className='cursor-pointer hover:scale-110 transition duration-300 ease-in-out rounded-sm border-[#e6e5eb6c]
-                            border-2'
-                            onClick={() => window.open('/scheduler2.png')}
+                        <Image alt="tictactoe2" src='/tictactoe2.png' width={500} height={500}
+                            className='cursor-pointer hover:scale-110 transition duration-300 ease-in-out rounded-sm'
+                            onClick={() => window.open('/tictactoe2.png')}
                         />
                     </div>
                     <p>
-                        Role: Co-Lead (Team of 2)
+                        Live Demo Link:
+                        <a href='https://greglobrigo.github.io/TicTacToe/' target='_blank' rel="noreferrer">
+                            <span className='cursor-pointer hover:underline transition duration-300 ease-in-out hover:text-[#fb844d]'> Click Here</span>
+                        </a>
+                        <br />
                     </p>
                     <p>
-                        Timeline: January 2023 - Present
+                        Role: Front End Developer (Solo)
                     </p>
                     <p>
-                        Project Type: Desktop App
+                        Timeline: August 9, 2021
                     </p>
                     <p>
-                        Tags: Desktop App, Scheduling, Automation, File Uploads
+                        Project Type: Game, Web App
                     </p>
                     <p>
-                        Technologies: Object Pascal, Delphi, Lazarus
+                        Tags: Game, Web App, TicTacToe
+                    </p>
+                    <p>
+                        Technologies: HTML, CSS, Vanilla JS
                     </p>
                     <p className='pb-[100px]'>
-                        Description: Generic Scheduler is the glue that makes all of our projects work.
-                        It&apos;s a desktop app that we built to automate the fetching of relevant data from our clients&apos; systems and
-                        uploads them to our cloud database. It&apos;s the backbone of our service which makes it possible for us to provide real time analytics and reports.
-                        The app is cross platform and can be run on Windows, Linux and Mac, while also being able to support low spec machines.
-                        The scheduler app is also capable of running multiple instances of itself and can be scheduled to run at a specific time.
-                    </p> */}
+                        Description: A simple TicTacToe game with a feature to track scores and move history after a match.
+                        The app also has sound effects that you can toggle on and off.
+                    </p>
                     <div className='flex xl:flex-row justify-center items-center flex-col gap-10'>
                     </div>
                 </div>
